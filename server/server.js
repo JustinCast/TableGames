26b4486607d007019b3eb5d71c21aecf4284f9a0
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-02-18 22:57:40 -0600

    feat: working on server.js on setting .env file

--- server/server.js
+++ server/server.js
@@ -1,30 +1,40 @@
+'use strict'
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { execute, subscribe } from 'graphql';
 const express = require("express");
 const expressGraphQL = require("express-graphql");
+import 'dotenv/config';
 const port = process.env.port || 3000;
 // TODO: importar aqui el o los schemas
 
 const app = express();
 
-app.use(
-  "/api/ql",
-  expressGraphQL({
-    schema: "", // TODO: importar el schema
-    graphiql: true
-  })
-);
+/** 
+ * Http graphql definition
+ * **/
+// app.use(
+//   "/api/ql",
+//   expressGraphQL({
+//     schema: "", // TODO: importar el schema
+//     graphiql: true
+//   })
+// );
 
 /** GraphQL Websocket definition **/
 SubscriptionServer.create({
-  schema,
+  //schema,
   execute,
   subscribe,
 }, {
-  server: server,
+  //server: server,
   path: '/api/ws',
 }, );
 
+
+
+console.log('Hello Node.js project.');
+
+console.log(process.env.MY_SECRET);
 app.listen(port, () => {
   console.log(`Running server on ${port}`);
 });
