0f74fbb6fd792bc02ddafce66d65beaf8d988e6a
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-02-19 22:00:57 -0600

    feat: working on session.js

--- server/models/session.js
+++ server/models/session.js
@@ -1,9 +1,67 @@
-const { GraphQLList, GraphQLObjectType, GraphQLInt } = require("graphql");
+const {
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLNonNull
+} = require("graphql");
+import player from "./player";
 
 const session = new GraphQLObjectType({
   name: "session",
   fields: () => ({
-    users: { type: GraphQLList },
-    index: { type: GraphQLInt }
+    users: { type: [player] },
+    index: { type: GraphQLInt },
+    uid: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    sessions: {
+      type: new GraphQLList(session),
+      /**
+       * TODO: implementar
+       */
+      resolve() {}
+    },
+    session: {
+      type: session,
+      args: {
+        uid: GraphQLString
+      },
+      /**
+       * TODO: implementar
+       * @param {*} parent
+       * @param {*} args
+       */
+      resolve(parent, args) {}
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    saveSession: {
+      type: session,
+      args: {
+        users: { type: new GraphQLNonNull(GraphQLList(player)) },
+        index: { type: new GraphQLNonNull(GraphQLInt) },
+        uid: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      /**
+       * TODO: implementar
+       * @param {*} parentValue 
+       * @param {*} args 
+       */
+      resolve(parentValue, args) {}
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: mutation
 });
