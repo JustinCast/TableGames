1bfc70cb6b7570e2d8cf5616c49bc6e1fcedc038
Luis González <luisgoncal98@gmail.com>
2019-03-06 09:42:39 -0600

    Creacción de servicios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './Style/app.css';
 import MainScreen from './Components/MainScreen/MainScreen'
+
+//here we go
+import {injector} from 'react-services-injector';
+import IndexServices from './Components/Services/IndexServices';
+injector.register(IndexServices);
+
 class App extends Component {
   render() {
     return (
