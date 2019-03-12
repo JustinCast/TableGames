526a861101398af8adbe21a96aa665f65ccd6c02
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-03-12 09:23:23 -0600

    feat: config index.js for working with apollo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import { ws_uri } from "./environments/env";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient, {} from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+//import { SubscriptionClient } from "subscriptions-transport-ws";
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from "react-apollo";
+import gql from 'graphql-tag';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create WebSocket client
+// const WSClient = new SubscriptionClient(ws_uri, {
+//   reconnect: true,
+//   connectionParams: {
+//     // Connection parameters to pass some validations
+//     // on server side during first handshake
+//   }
+// });
 
-// Apollo config
-//import ApolloClient from "apollo-boost";
-//export default client = new ApolloClient({
-//   uri: 'http://localhost:3000/root'
-//});
+const GraphQLClient = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000/root' }),
+  cache: new InMemoryCache()
+});
+
+
+GraphQLClient.query({ query: gql`{ players { name } }` }).then(console.log);
+ReactDOM.render(
+  <ApolloProvider client={GraphQLClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+serviceWorker.register();
