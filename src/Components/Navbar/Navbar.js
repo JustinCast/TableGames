e9de6ad1b7e5d3d450ceaa1524213505014d1a17
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2019-03-16 16:02:44 -0600

    feat: working in loginService

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import './Navbar.scss'
 import Button from '@material-ui/core/Button';
-import {injector} from 'react-services-injector';
 class Navbar extends Component {
     render() {
-        const {LoginService}= this.services;
         return (
             <div className="nav shadow-sm">
                 <div className="nav-element-left">
                     <div className="user-name">
-                        {LoginService.userName} 
+                        {JSON.parse(localStorage.getItem('actualUser')).name} 
                     </div>
                 </div>
                 <div className="nav-element-rigth">
@@ -21,4 +19,4 @@ class Navbar extends Component {
     }
 }
 
-export default injector.connect(Navbar, {toRender: ['LoginService']});
+export default Navbar;
--- src/Components/Services/SessionService.js
+++ src/Components/Services/SessionService.js
@@ -1,5 +1,6 @@
 import {Service} from 'react-services-injector';
-
+import { GraphQLClient } from "../../index";
+import gql from "graphql-tag";
 class SessionService extends Service {
     
     createSession(session){
