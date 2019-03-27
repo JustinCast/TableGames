ff158b54727fcb430ffaafbe0aed35e5c47b809b
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-03-26 23:02:44 -0600

    refactor: move saveState functions for both games to logic-index

--- server/logic/logic-index.js
+++ server/logic/logic-index.js
@@ -17,3 +17,60 @@ export function fillList(size) {
   }
   return array;
 }
+
+
+// some firebase funcs
+export function saveStateGame(game,token){
+  if(token === undefined) // Start default state game
+    return new Promise(resolve => resolve(
+      db.collection("stateGame").add(game)
+      .then(function(docRef) {
+        return docRef.id;
+      })
+      .catch(function(error) {
+          console.error("Error adding document: ", error);
+      })
+    ));
+
+  else // Update state game
+    return new Promise(resolve => resolve(
+      db.collection("stateGame").doc(token).set(game)
+      .then(function(docRef) {
+        return docRef.id;
+      })
+      .catch(function(error) {
+          console.error("Error adding document: ", error);
+      })
+    ));
+  
+}
+
+export function saveMemoryInitialGameState(gameData, token) {
+  if(!token)
+    return new Promise(resolve => resolve(
+      db.collection("stateGame")
+      .add(gameData)
+      .then(docRef => {
+        return docRef.id
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      })
+    ));
+  else
+    return new Promise(resolve => resolve(
+      db.collection("stateGame")
+      .doc(token)
+      .set(game)
+      .then(docRef => {
+        return docRef.id;
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      })
+    ));
+}
+
+export function changeActualUser(params) {
+  
+}
--- server/schemas/query-mutations.js
+++ server/schemas/query-mutations.js
@@ -9,6 +9,7 @@ import {
 } from "graphql";
 
 import { fillDefaultCheck } from '../logic/checkers';
+import { saveMemoryInitialGameState, saveStateGame } from '../logic/logic-index';
 // firestore instance
 import db from "../config/config";
 // import schemas
@@ -158,56 +159,3 @@ const mutation = new GraphQLObjectType({
 
 // exporting RootQuery and mutations
 export { RootQuery, mutation };
-
-
-// some firebase funcs
-function saveStateGame(game,token){
-  if(token === undefined) // Start default state game
-    return new Promise(resolve => resolve(
-      db.collection("stateGame").add(game)
-      .then(function(docRef) {
-        return docRef.id;
-      })
-      .catch(function(error) {
-          console.error("Error adding document: ", error);
-      })
-    ));
-
-  else // Update state game
-    return new Promise(resolve => resolve(
-      db.collection("stateGame").doc(token).set(game)
-      .then(function(docRef) {
-        return docRef.id;
-      })
-      .catch(function(error) {
-          console.error("Error adding document: ", error);
-      })
-    ));
-  
-}
-
-async function saveMemoryInitialGameState(gameData, token) {
-  if(!token)
-    return new Promise(resolve => resolve(
-      db.collection("stateGame")
-      .add(gameData)
-      .then(docRef => {
-        return docRef.id
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      })
-    ));
-  else
-    return new Promise(resolve => resolve(
-      db.collection("stateGame")
-      .doc(token)
-      .set(game)
-      .then(docRef => {
-        return docRef.id;
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      })
-    ));
-}
