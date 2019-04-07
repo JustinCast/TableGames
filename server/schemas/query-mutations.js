74357dd63e1f0aa443ed095b5e376cbc858b600f
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-04-07 13:54:01 -0500

    minimal: minimal changes before merge}

--- server/schemas/query-mutations.js
+++ server/schemas/query-mutations.js
@@ -203,7 +203,6 @@ const mutation = new GraphQLObjectType({
               checkSelection(data.input.stateGameId,JSON.parse(data.input.object)).then( res =>{ // Corresponds to the current player ?
                   if(res){
                     getChecker(data.input.stateGameId).then(checker => {
-<<<<<<< HEAD
         
                       if(isMovementValid( // Is a valid movement ?
                         checker,
@@ -212,13 +211,6 @@ const mutation = new GraphQLObjectType({
                         data.input.player)){
                           //saveStateGame(game,data.input.stateGameId);
                         }
-=======
-                      isMovementValid( // Is a valid movement ?
-                        checker,
-                        JSON.parse(data.input.object), 
-                        data.input.stateGameId,
-                        data.input.player).then( () => {return data.input})
->>>>>>> luis_dev
                     })
                     
                   }
