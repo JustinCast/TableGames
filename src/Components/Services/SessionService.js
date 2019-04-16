b3f458f3c2c4bc8bc24bc9c4368d4d1ba4b5bbe0
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-04-15 20:52:20 -0600

    minimal changes were added

--- src/Components/Services/SessionService.js
+++ src/Components/Services/SessionService.js
@@ -1,79 +1,74 @@
-import { Service } from "react-services-injector";
+import { Service } from 'react-services-injector';
 import { GraphQLClient } from "../../index";
 import gql from "graphql-tag";
 class SessionService extends Service {
-  addUser(session) {
-    let userList = [];
-    if (!session.isMachine) {
-      let creatorUser = {
-        name: session.users[0].name,
-        email: session.users[0].email,
-        wonGames: session.users[0].wonGames,
-        lostGames: session.users[0].lostGames,
-        tiedGames: session.users[0].tiedGames,
-        uid: session.users[0].uid
-      };
 
-      let actualUser = {
-        name: JSON.parse(localStorage.getItem("actualUser")).name,
-        email: JSON.parse(localStorage.getItem("actualUser")).email,
-        wonGames: JSON.parse(localStorage.getItem("actualUser")).wonGames,
-        lostGames: JSON.parse(localStorage.getItem("actualUser")).lostGames,
-        tiedGames: JSON.parse(localStorage.getItem("actualUser")).tiedGames,
-        uid: JSON.parse(localStorage.getItem("actualUser")).uid
-      };
+  addUser(session){
+    let userList=[];
 
-      userList.push(creatorUser);
-      userList.push(actualUser);
+    let creatorUser = {
+      name: session.users[0].name,
+      email: session.users[0].email,
+      wonGames: session.users[0].wonGames,
+      lostGames: session.users[0].lostGames,
+      tiedGames: session.users[0].tiedGames,
+      uid: session.users[0].uid
+    }
 
-      GraphQLClient.mutate({
-        variables: {
-          input: {
-            users: userList,
-            sid: session.sid, // TODO: get uid of Actual User
-            game: session.game,
-            difficulty: session.difficulty,
-            isMachine: session.isMachine,
-            name: session.name,
-            gameSize: session.gameSize,
-            stateGameId: session.stateGameId
-          }
-        },
-        mutation: gql`
+    let actualUser = {
+      name: JSON.parse(localStorage.getItem('actualUser')).name,
+      email: JSON.parse(localStorage.getItem('actualUser')).email,
+      wonGames: JSON.parse(localStorage.getItem('actualUser')).wonGames,
+      lostGames: JSON.parse(localStorage.getItem('actualUser')).lostGames,
+      tiedGames: JSON.parse(localStorage.getItem('actualUser')).tiedGames,
+      uid: JSON.parse(localStorage.getItem('actualUser')).uid
+    }
+    
+    userList.push(creatorUser);
+    userList.push(actualUser);
+
+    GraphQLClient.mutate({
+      variables: {
+        input: {
+          users: userList,
+          sid: session.sid, // TODO: get uid of Actual User
+          game: session.game,
+          difficulty: session.difficulty,
+          isMachine: session.isMachine,
+          name: session.name,
+          gameSize: session.gameSize,
+          stateGameId: session.stateGameId
+        }
+      },
+      mutation: gql`
           mutation saveSession($input: SessionInputType!) {
             saveSession(input: $input) {
               stateGameId
             }
           }
         `
-      })
-        .then(data => {
-          localStorage.setItem(
-            "stateGameId",
-            JSON.stringify(data.data.saveSession.stateGameId)
-          );
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
+    }).then(data => {
+      localStorage.setItem('stateGameId', JSON.stringify(data.data.saveSession.stateGameId));
+    })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   createSession(session) {
     let actualUser = {
-      name: JSON.parse(localStorage.getItem("actualUser")).name,
-      email: JSON.parse(localStorage.getItem("actualUser")).email,
-      wonGames: JSON.parse(localStorage.getItem("actualUser")).wonGames,
-      lostGames: JSON.parse(localStorage.getItem("actualUser")).lostGames,
-      tiedGames: JSON.parse(localStorage.getItem("actualUser")).tiedGames,
-      uid: JSON.parse(localStorage.getItem("actualUser")).uid
-    };
+      name: JSON.parse(localStorage.getItem('actualUser')).name,
+      email: JSON.parse(localStorage.getItem('actualUser')).email,
+      wonGames: JSON.parse(localStorage.getItem('actualUser')).wonGames,
+      lostGames: JSON.parse(localStorage.getItem('actualUser')).lostGames,
+      tiedGames: JSON.parse(localStorage.getItem('actualUser')).tiedGames,
+      uid: JSON.parse(localStorage.getItem('actualUser')).uid
+    }
     let users = [actualUser];
 
     if (session.isMachine) {
       users.push(null);
     }
-    console.log(session);
     GraphQLClient.mutate({
       variables: {
         input: {
@@ -87,9 +82,10 @@ class SessionService extends Service {
         }
       },
       mutation: gql`
-        mutation saveSession($input: SessionInputType!) {
-          saveSession(input: $input) {
-            stateGameId
+          mutation saveSession($input: SessionInputType!) {
+            saveSession(input: $input) {
+              stateGameId
+            }
           }
         `
     }).then(data => {
@@ -97,18 +93,14 @@ class SessionService extends Service {
       localStorage.setItem('stateGameId', JSON.stringify(data.data.saveSession.stateGameId));
       console.log(localStorage.getItem("stateGameId")+" despues de guardarlo el local ");
     })
-      .then(data => {
-        localStorage.setItem(
-          "stateGameId",
-          JSON.stringify(data.data.saveSession.stateGameId)
-        );
-      })
       .catch(error => {
         console.log(error);
       });
   }
 }
 
-SessionService.publicName = "SessionService";
+
+SessionService.publicName = 'SessionService';
+
 
 export default SessionService;
