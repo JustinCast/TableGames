04eb8dc6dcb934e925e0078a6202f2df9cd922ce
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2019-08-17 16:17:03 -0600

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,6 @@
 import firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyBUDHajC_WsEn0u3skyDuQ_XPehC61o-lY",
-  authDomain: "fir-auth-react-64f8d.firebaseapp.com",
-  databaseURL: "https://fir-auth-react-64f8d.firebaseio.com",
-  projectId: "fir-auth-react-64f8d",
-  storageBucket: "fir-auth-react-64f8d.appspot.com",
-  messagingSenderId: "460343751130"
 };
 firebase.initializeApp(config);
 
